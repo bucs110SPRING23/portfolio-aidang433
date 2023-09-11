6581962de38a0b8a7feb04446ccb8fb3e471e1cd
Aidan Gonzalez <gonzalezaidan433@gmail.com>
2023-09-11 17:58:28 -0400

    yuh

---
+++ ch01/exercises/currency.py
@@ -0,0 +1,5 @@
+rate = float(input("Enter Exchange Rate from Euros to Dollars: "))
+amount = float(input("Enter Amount Being Exchanged: "))
+total = amount * rate
+result = total - 3
+print("A 3 dollar serivce fee has been charged, You will recieve: ",result)
---
+++ ch02/exercises/drawing.py
@@ -0,0 +1,12 @@
+import turtle 
+donatello = turtle.Turtle()
+screen = turtle.Screen()
+donatello.color("purple")
+donatello.forward(50)
+donatello.up()
+donatello.forward(50)
+donatello.down()
+donatello.left(90)
+
+
+screen.exitonclick()
